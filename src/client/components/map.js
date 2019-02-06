717e52b9624b706ea159a854cd3af1cefb020dea
Brian Hon <brianwhon@gmail.com>
2019-02-05 20:45:05 -0500

    webpack set up for js only, app divided into main component with one child component, and content rendering properly in both modularized components

---
+++ src/client/components/map.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>MAP COMPONENT</h1>
+            </div>
+        )
+    }
+}
+
+export default Map;
---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+import render from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
